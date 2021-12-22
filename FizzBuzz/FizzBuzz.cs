16c0f6f336ef56f4c927e8f49ea384a831298b44
Lukasz Zaczyk <zaczyk.l@gmail.com>
2021-12-22 20:11:00 +0100

    first commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {       
+        public string FizzBuzzOutput (int inputNumber)
+        {
+            if (inputNumber % 3 == 0 && inputNumber % 5 == 0)
+                return "FizzBuzz";            
+            if (inputNumber%3==0)
+                return "Fizz";            
+            if (inputNumber%5==0)
+                return "Buzz";            
+                        
+                return inputNumber.ToString();            
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            try
+            {
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                while (true)
+                {
+                    Console.WriteLine("Wprowadź liczbę. Jeżeli chcesz zakończyć wprowadź znak T.\n");
+                    Console.WriteLine(fizzBuzz.FizzBuzzOutput(GetValidateUserNumber())+"\n");                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static int GetValidateUserNumber()
+        {
+            string input = Console.ReadLine();
+            int result;
+            
+            while (!int.TryParse(input, out result))
+            {
+                if (input.ToUpper() == "T")                
+                    Environment.Exit(0);
+                
+                Console.WriteLine("Wprowadzono nieprawidłowe dane, spróbuj ponownie.");
+                input = Console.ReadLine();
+            }
+            return result;
+        }
+    }
+}
